330607707ffcacdc5bfe13bab8606e37993c3c79
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-08-14 16:57:11 +0200

    Set default return to array on method get.

--- src/AbstractRepository.php
+++ src/AbstractRepository.php
@@ -24,7 +24,7 @@ abstract class AbstractRepository implements IRepository
     }
     public function get()
     {
-        $data = Cache::get($this->getKey());
+        $data = Cache::get($this->getKey(),[]);
         return $data;
     }
 
